3d52118a768d63e17e47c4c194417094c64084f6
Mariam Hammoud <mhamm049@uottawa.ca>
2020-05-28 11:28:33 -0400

    Update MOdule 02

--- SEG3125-Module2-Grocery/scripts/groceries.js
+++ SEG3125-Module2-Grocery/scripts/groceries.js
@@ -1,34 +1,108 @@
-	
+
 // Array of products, each product is an object with different fieldset
-// A set of ingredients should be added to products		 
+// A set of ingredients should be added to products
 
 var products = [
 	{
 		name: "brocoli",
 		vegetarian: true,
 		glutenFree: true,
+		organic: false,
 		price: 1.99
 	},
 	{
 		name: "bread",
 		vegetarian: true,
 		glutenFree: false,
+		organic: false,
 		price: 2.35
 	},
 	{
 		name: "salmon",
 		vegetarian: false,
 		glutenFree: true,
+		organic: true,
 		price: 10.00
+	},
+	{
+		name: "peanut butter",
+		vegetarian: true,
+		glutenFree: true,
+		organic: true,
+		price: 3.50
+	},
+	{
+		name: "yogurt",
+		vegetarian: true,
+		glutenFree: true,
+		organic: false,
+		price: 5.60
+	},
+	{
+		name: "chicken breast",
+		vegetarian: false,
+		glutenFree: true,
+		organic: false,
+		price: 13.50
+	},
+	{
+		name: "spaghetti",
+		vegetarian: true,
+		glutenFree: false,
+		organic: false,
+		price: 1.50
+	},
+	{
+		name: "tomatoes",
+		vegetarian: true,
+		glutenFree: true,
+		price: 4.00
+	},
+	{
+		name: "beef lasagna",
+		vegetarian: false,
+		glutenFree: false,
+		organic: false,
+		price: 9.99
+	},
+	{
+		name: "tortillas",
+		vegetarian: true,
+		glutenFree: false,
+		organic: false,
+		price: 4.50
 	}
 ];
-	
+
 
 
 // given restrictions provided, make a reduced list of products
 // prices should be included in this list, as well as a sort based on price
 
-function restrictListProducts(prods, restriction) {
+function restrictListProducts(prods, restrictTags) {
+
+	var product_names = prods;
+
+	if (restrictTags.includes("Vegan")){
+		var temp = product_names.filter(function(elem){ return elem.vegetarian == true; });
+		product_names = temp;
+	}
+	if (restrictTags.includes("Gluten Free")){
+		var temp = product_names.filter(function(elem){ return elem.glutenFree == true; });
+		product_names = temp;
+	}
+	if (restrictTags.includes("Organic")){
+		var temp = product_names.filter(function(elem){ return elem.organic == true; });
+		product_names = temp;
+	}
+
+	return product_names;
+
+	// Je n'ai pas trouvé de manière plus optimisée de faire ça, je ne sais pas comment
+	// faire correspondre les strings "Vegan" et les attributs .vegetarian
+	// En Java, j'aurais fait une enum.
+
+	/*
 	let product_names = [];
 	for (let i=0; i<prods.length; i+=1) {
 		if ((restriction == "Vegetarian") && (prods[i].vegetarian == true)){
@@ -42,6 +116,7 @@ function restrictListProducts(prods, restriction) {
 		}
 	}
 	return product_names;
+	*/
 }
 
 // Calculate the total price of items, with received parameter being a list of products
--- SEG3125-Module2-Grocery/scripts/main.js
+++ SEG3125-Module2-Grocery/scripts/main.js
@@ -23,26 +23,26 @@ function openInfo(evt, tabName) {
 }
 
 
-	
+
 // generate a checkbox list from a list of products
 // it makes each product name as the label for the checkbos
 
 function populateListProductChoices(slct1, slct2) {
-    var s1 = document.getElementById(slct1);
-    var s2 = document.getElementById(slct2);
-	
+    var s1 = document.getElementById(slct1);// Liste des restrictions
+    var s2 = document.getElementById(slct2);// Liste des produits filtrés à remplir
+
 	// s2 represents the <div> in the Products tab, which shows the product list, so we first set it empty
     s2.innerHTML = "";
-		
+
 	// obtain a reduced list of products based on restrictions
     var optionArray = restrictListProducts(products, s1.value);
 
 	// for each item in the array, create a checkbox element, each containing information such as:
 	// <input type="checkbox" name="product" value="Bread">
 	// <label for="Bread">Bread/label><br>
-		
+
 	for (i = 0; i < optionArray.length; i++) {
-			
+
 		var productName = optionArray[i];
 		// create the checkbox and add in HTML DOM
 		var checkbox = document.createElement("input");
@@ -50,45 +50,112 @@ function populateListProductChoices(slct1, slct2) {
 		checkbox.name = "product";
 		checkbox.value = productName;
 		s2.appendChild(checkbox);
-		
+
 		// create a label for the checkbox, and also add in HTML DOM
 		var label = document.createElement('label')
 		label.htmlFor = productName;
 		label.appendChild(document.createTextNode(productName));
 		s2.appendChild(label);
-		
+
 		// create a breakline node and add in HTML DOM
-		s2.appendChild(document.createElement("br"));    
+		s2.appendChild(document.createElement("br"));
 	}
 }
-	
+
 // This function is called when the "Add selected items to cart" button in clicked
-// The purpose is to build the HTML to be displayed (a Paragraph) 
+// The purpose is to build the HTML to be displayed (a Paragraph)
 // We build a paragraph to contain the list of selected items, and the total price
 
 function selectedItems(){
-	
+
 	var ele = document.getElementsByName("product");
 	var chosenProducts = [];
-	
+
 	var c = document.getElementById('displayCart');
 	c.innerHTML = "";
-	
+
 	// build list of selected item
 	var para = document.createElement("P");
 	para.innerHTML = "You selected : ";
 	para.appendChild(document.createElement("br"));
-	for (i = 0; i < ele.length; i++) { 
+	for (i = 0; i < ele.length; i++) {
 		if (ele[i].checked) {
 			para.appendChild(document.createTextNode(ele[i].value));
 			para.appendChild(document.createElement("br"));
 			chosenProducts.push(ele[i].value);
 		}
 	}
-		
+
 	// add paragraph and total price
 	c.appendChild(para);
 	c.appendChild(document.createTextNode("Total Price is " + getTotalPrice(chosenProducts)));
-		
+
 }
 
+
+// Restrictions list in Array
+var restrictOptions = ["Vegan","Gluten Free","Organic","None"];
+
+// Makes a list of checkboxes with the possible restrictions in the list
+function makeRestrictList(restrict){
+
+	console.log("Enter makeRestrictList :");
+	console.log(restrict);
+
+	restList = document.getElementById(restrict);
+	restList.innerHTML = "";
+
+	for(i = 0; i < restrictOptions.length; i++){
+		var checkbox = document.createElement("input");
+		checkbox.type = "checkbox";
+		checkbox.name = "restriction";
+		checkbox.value = restrictOptions[i];
+		//checkbox.oninput = "buildFiltersList(restrictFilter, 'restriction'); populateListProductChoices(restrictFilter,'displayProduct');"
+		restList.appendChild(checkbox);
+
+		var label = document.createElement('label');
+		label.htmlFor = restrictOptions[i];
+		label.appendChild(document.createTextNode(restrictOptions[i]));
+		restList.appendChild(label)
+
+		restList.appendChild(document.createElement('br'));
+	}
+}
+
+var restrictFilter = [];
+
+// Returns the list of restrictions to filter the products
+function buildFiltersList(restrictFilter,checkbox){
+	boxList = document.getElementsByName(checkbox);// List des checkbox restrictions
+
+	for(i = 0; i < boxList.length; i++){
+		if(boxList[i].checked){
+			if (!restrictFilter.includes(boxList[i].value)){
+				restrictFilter.push(boxList[i].value);
+			}
+		} else {
+			if (restrictFilter.includes(boxList[i].value)){
+				var newList = restrictFilter.filter(function(value) { return value != boxList[i].value;});
+				restrictFilter = newList;
+			}
+		}
+	}
+
+}
+
+
+function displayRestrict(){
+	disp = document.getElementById("displayRestList");
+	disp.innerHTML = "";
+
+	for(i = 0; i < restrictFilter.length; i++;){
+		var elem = document.createTextNode(restrictFilter[i]);
+		disp.appendChild(elem);
+		disp.appendChild(document.createElement("br"));
+
+	}
+}
+
+function hello(){
+	console.log("Hello ^^!");
+}
